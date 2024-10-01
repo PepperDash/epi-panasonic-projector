f2c6df3aaf3cd046174b0e2e67a66aa25cf6260b
jdevito <jdevito@pepperdash.com>
2024-10-01 11:58:54 -0500

    refactor: rename config.cs and factory.cs files

---
+++ src/PanasonicProjectorConfig.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using PepperDash.Essentials.Core;
+
+namespace PepperDash.Essentials.Plugins.Display.Panasonic.Projector
+{
+    /// <summary>
+    /// Plugin device configuration object
+    /// </summary>	
+    public class PanasonicProjectorConfig
+	{
+        [JsonProperty("control")]
+        public EssentialsControlPropertiesConfig Control { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("warmupTimeInSeconds")]
+        public long WarmupTimeInSeconds { get; set; }
+
+        [JsonProperty("cooldownTimeInSeconds")]
+        public long CooldownTimeInSeconds { get; set; }
+	}
+}
---
+++ src/PanasonicProjectorFactory.cs
@@ -0,0 +1,54 @@
+﻿using PepperDash.Core;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Config;
+using System.Collections.Generic;
+
+namespace PepperDash.Essentials.Plugins.Display.Panasonic.Projector
+{
+    /// <summary>
+    /// Plugin device factory for devices that use IBasicCommunication
+    /// </summary>
+    public class PanasonicProjectorFactory : EssentialsPluginDeviceFactory<PanasonicProjectorController>
+    {
+        /// <summary>
+        /// Plugin device factory constructor
+        /// </summary>
+        public PanasonicProjectorFactory()
+        {
+            // Set the minimum Essentials Framework Version
+            MinimumEssentialsFrameworkVersion = "1.11.1";
+
+            // In the constructor we initialize the list with the typenames that will build an instance of this device
+            TypeNames = new List<string> { "panasonicProjector" };
+        }
+
+        /// <summary>
+        /// Builds and returns an instance of EssentialsPluginDeviceTemplate
+        /// </summary>
+        /// <param name="dc">device configuration</param>
+        public override EssentialsDevice BuildDevice(DeviceConfig dc)
+        {
+            Debug.Console(1, "[{0}] Factory Attempting to create new device from type: {1}", dc.Key, dc.Type);
+
+            // get the plugin device properties configuration object & check for null 
+            var propertiesConfig = dc.Properties.ToObject<PanasonicProjectorConfig>();
+            if (propertiesConfig == null)
+            {
+                Debug.Console(0, "[{0}] Factory: failed to read properties config for {1}", dc.Key, dc.Name);
+                return null;
+            }
+
+            // attempt build the plugin device comms device & check for null
+            var comms = CommFactory.CreateCommForDevice(dc);
+
+            if (comms != null)
+            {
+                return new PanasonicProjectorController(dc.Key, dc.Name, propertiesConfig, comms);
+            }
+
+            Debug.Console(1, "[{0}] Factory Notice: No control object present for device {1}", dc.Key, dc.Name);
+            return null;
+        }
+    }
+}
+

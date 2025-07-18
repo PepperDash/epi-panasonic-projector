5af377099c92a3a13cbae618199177eebfd0e30f
Aviv Cohn <acohn@pepperdash.com>
2025-07-18 17:10:17 -0400

    feat: add ExtendedDisplayJoinMap class

---
+++ src/ExtendedDisplayJoinMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using PepperDash.Essentials.Core.Bridges;
+using PepperDash.Essentials.Core;
+
+namespace PanasonicProjectorEpi
+{
+    public class ExtendedDisplayJoinMap : DisplayControllerJoinMap
+    {
+        [JoinName("Connect")]
+        public JoinDataComplete Connect;
+
+        [JoinName("Disconnect")]
+        public JoinDataComplete Disconnect;
+    }
+}

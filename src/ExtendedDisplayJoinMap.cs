e2f4e99ee3460dfcb6b682bac0fca6dd925dc4b7
Aviv Cohn <acohn@pepperdash.com>
2025-07-18 17:25:17 -0400

    fix: add join data for connect and disconnect

--- src/ExtendedDisplayJoinMap.cs
+++ src/ExtendedDisplayJoinMap.cs
@@ -11,9 +11,37 @@ namespace PanasonicProjectorEpi
     public class ExtendedDisplayJoinMap : DisplayControllerJoinMap
     {
         [JoinName("Connect")]
-        public JoinDataComplete Connect;
+        public JoinDataComplete Connect = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 255,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Connect Socket",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
 
         [JoinName("Disconnect")]
-        public JoinDataComplete Disconnect;
+        public JoinDataComplete Disconnect = new JoinDataComplete(
+            new JoinData
+            {
+                JoinNumber = 256,
+                JoinSpan = 1
+            },
+            new JoinMetadata
+            {
+                Description = "Disconnect Socket",
+                JoinCapabilities = eJoinCapabilities.ToSIMPL,
+                JoinType = eJoinType.Digital
+            });
+
+
+        public ExtendedDisplayJoinMap(uint joinStart)
+            : base(joinStart, typeof(ExtendedDisplayJoinMap))
+        {
+        }
     }
 }
